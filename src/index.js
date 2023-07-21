66eaf93d3f4dbc51be75d18d5666b655b0db6933
xu <devusexu@gmail.com>
2023-07-21 21:31:11 +0800

    change dependency to author's package.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import { createRoot } from 'react-dom/client';
 
-const domNode = document.getElementById('app');
-const root = createRoot(domNode);
-root.render(<App />);
-// render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
 
 
 if (module.hot) {
